f250fec18d214168a9b5bdeb0614c5983d043fee
Matt-Henr <mateush.study@gmail.com>
2023-09-05 09:53:56 -0300

    Changes for Linux

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         'peewee',
-        'psycopg2',
+        'psycopg2-binary',
         'mercadopago',
         'pillow',
-        'pywin32',
         'fpdf',
         'requests'
     ],
