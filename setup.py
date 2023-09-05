6d1f87ef2fc6b487e3bee3146bfb11fb9b548e0f
Matt-Henr <mateush.study@gmail.com>
2023-09-05 09:53:13 -0300

    Missing files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bank_project',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your@email.com',
+    description='Description of your package',
+    packages=find_packages(),
+    install_requires=[
+        'peewee',
+        'psycopg2',
+        'mercadopago',
+        'pillow',
+        'pywin32',
+        'fpdf',
+        'requests'
+    ],
+)
