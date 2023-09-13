806394e082d9f0d3ec8a864a6c003559789d6e98
Miguel_Ribeiro <miguel.a.silva@ufv.com>
2023-09-13 19:04:11 -0300

    JSON na tabela de transação

--- src/controller/mercado_pago.py
+++ src/controller/mercado_pago.py
@@ -61,7 +61,10 @@ class PixMercadoPago(PixInterface):
                 date_approved = payment["response"]["date_approved"]
                 total_paid_amount = payment["response"]["transaction_details"]["total_paid_amount"]
 
-                TransacaoDAO.insert_trasacao(transaction_id, account_id, bank_name, date_approved, total_paid_amount)
+                JSON = payment["response"]
+
+
+                TransacaoDAO.insert_trasacao(transaction_id, account_id, bank_name, date_approved, total_paid_amount, JSON)
 
         else:
             self.status = "pending"
